9fc2252d5e5a2abd11b1ab6f5ed184eb25f8fe96
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-23 21:28:57 -0400

    challenge done

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -3,6 +3,10 @@ package com.dsimon;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(FlourPack.canPack(1,0,4) == false);
+        System.out.println(FlourPack.canPack(1, 0, 5) == true);
+        System.out.println(FlourPack.canPack(0, 5, 4) == true);
+        System.out.println(FlourPack.canPack(2, 2, 11) == true);
+        System.out.println(FlourPack.canPack(-3, 2, 12) == false);
     }
 }
